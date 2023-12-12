54e9ccb3242bfff7c663243c2e4f284a246272c6
namratasivakumar <141306824+namratasivakumar@users.noreply.github.com>
2023-12-12 16:49:03 +0530

    Update Token

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def readCommand():
 
     headers = {
         "Content-Type": "application/json",
-        "Authorization": "Bearer eyJ0eXAiOiAiVENWMiJ9.bUtSVVJBZmhwZUZBMDVGNS13ejJNTnFkUmJn.OGE1NjM1N2EtOWVlYi00M2VkLTg0MjAtYTgxZjQ1N2VmMTRj"
+        "Authorization": "Bearer eyJ0eXAiOiAiVENWMiJ9.TjJleXdBSVFxMUtvdjh3M1ljajMtdXpfYVJ3.OWQ4YTkzM2YtYzlhYi00YWE2LWE0YWYtZmRlNjA3NmM1YmY0"
     }
     build_id = "ProductDev_Rmp_CmpServices_UploadSimulator"
     response = requests.post(url, headers=headers, data=json.dumps({"buildType": {"id": build_id}}), verify=False)
