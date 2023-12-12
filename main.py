74b8d88035e5e5251cbcbb6a0d1ce118e07f7fe5
namratasivakumar <141306824+namratasivakumar@users.noreply.github.com>
2023-12-12 17:20:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,14 +7,18 @@ app = FastAPI()
 
 @app.post("/")
 def readCommand():
-    url = "http://tpteamcity.corp.theplatform.com/app/rest/buildQueue"
+    url = "https://tpteamcity.corp.theplatform.com/app/rest/buildQueue"
 
     headers = {
         "Content-Type": "application/json",
         "Authorization": "Bearer eyJ0eXAiOiAiVENWMiJ9.TjJleXdBSVFxMUtvdjh3M1ljajMtdXpfYVJ3.OWQ4YTkzM2YtYzlhYi00YWE2LWE0YWYtZmRlNjA3NmM1YmY0"
     }
     build_id = "ProductDev_Rmp_CmpServices_UploadSimulator"
-    response = requests.post(url, headers=headers, data=json.dumps({"buildType": {"id": build_id}}), verify=False)
+    try:
+        response = requests.post(url, headers=headers, data=json.dumps({"buildType": {"id": build_id}}), verify=False)
+    except Exception as e:
+        print("EXCEPTION:", e)
+        
     print(response.text)
     if response.status_code == 200:
         return {"Hello": "World"}
